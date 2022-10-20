53f76bf9aaffaf6abe777c31ba59a96097558894
Kevin Lee <jhssttj@hotmail.com>
2022-10-20 12:30:40 -0600

    Refactor more functions with post gres

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -20,16 +20,9 @@ const pool = new Pool({
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithEmail = function(email) {
-  let user;
-  for (const userId in users) {
-    user = users[userId];
-    if (user.email.toLowerCase() === email.toLowerCase()) {
-      break;
-    } else {
-      user = null;
-    }
-  }
-  return Promise.resolve(user);
+  return pool.query(`SELECT * FROM users WHERE email = $1`,[email])
+    .then((result) => result.rows[0])
+    .catch((err) => null)
 }
 exports.getUserWithEmail = getUserWithEmail;
 
@@ -39,7 +32,9 @@ exports.getUserWithEmail = getUserWithEmail;
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithId = function(id) {
-  return Promise.resolve(users[id]);
+  return pool.query(`SELECT * FROM users WHERE id = $1`,[id])
+    .then((result) => result.rows[0])
+    .catch((err) => null)
 }
 exports.getUserWithId = getUserWithId;
 
@@ -50,10 +45,10 @@ exports.getUserWithId = getUserWithId;
  * @return {Promise<{}>} A promise to the user.
  */
 const addUser =  function(user) {
-  const userId = Object.keys(users).length + 1;
-  user.id = userId;
-  users[userId] = user;
-  return Promise.resolve(user);
+  return pool
+    .query(`INSERT INTO users (name, email, password) VALUES ($1,$2,$3) RETURNING *;`,[user.name, user.email, user.password])
+    .then((result) => result.rows[0])
+    .catch((err) => err);
 }
 exports.addUser = addUser;
 
@@ -80,13 +75,8 @@ exports.getAllReservations = getAllReservations;
  const getAllProperties = (options, limit = 10) => {
   return pool
     .query(`SELECT * FROM properties LIMIT $1`, [limit])
-    .then((result) => {
-      console.log(result.rows);
-      return result.rows;
-    })
-    .catch((err) => {
-      console.log(err.message);
-    });
+    .then((result) => {return result.rows})
+    .catch((err) => console.log(err.message));
 };
 exports.getAllProperties = getAllProperties;
 
